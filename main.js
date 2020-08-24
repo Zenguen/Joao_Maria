49e74dde47524a2cc5040e0170c18b8ca63efa71
Zenguen <gabrielnakazatodasilva@gmail.com>
2020-08-24 09:50:42 -0300

    Deixando Menos Feio

--- main.js
+++ main.js
@@ -1,105 +1,105 @@
-var tbl = document.getElementById("tabela");
-var Alunos = ["João", "Maria"];
-var mes = [];
-var row;
-var cell;
-var macasLevadas = 0;
-var perasLevadas = 0;
+const tbl = document.getElementById("tabela");
+const mes = [];
+const Alunos = ["João", "Maria"];
+const sortearMelhorAluno = () => Alunos[NumAluno = Math.floor(Math.random() * Alunos.length)];
+const criarNovoDia = () => {
+    return {
+            peras:0,
+            macas:0,
+            melhorAluno:sortearMelhorAluno(),
+            frutas:[],
 
+            sortearQtdFrutas()
+            {
+                while (this.macas == 0 && this.peras == 0)
+                {
+                this.peras = parseInt(Math.random() * (6));
+                this.macas = parseInt(Math.random() * (6));
+                }
+            },
 
-for (let i = 0; i < 30; i++) {
-    NumAluno = Math.floor(Math.random() * Alunos.length);
-
-    let novoDia = {};
-    novoDia.peras = 0;
-    novoDia.macas = 0;
-
-    while (novoDia.macas == 0 && novoDia.peras == 0) {
-        novoDia.peras = parseInt(Math.random() * (6));
-        novoDia.macas = parseInt(Math.random() * (6));
-    }
-    novoDia.melhorAluno = Alunos[NumAluno];
-
-
-    novoDia.professora = [];
-
-    if (novoDia.melhorAluno == "Maria") {
-        let qtsPeras = novoDia.peras;
-        let qtsMacas = novoDia.macas;
-        for (let j = 0; j < 3; j++) {
-            if (qtsPeras > 0) {
-                novoDia.professora[j] = "Pera";
-                qtsPeras--;
-            } else
-            if (qtsMacas > 0) {
-                novoDia.professora[j] = "Maçã";
-                qtsMacas--;
-            }
-
-        }
-    } else
-    if (novoDia.melhorAluno == "João") {
-        let qtsPeras = novoDia.peras;
-        let qtsMacas = novoDia.macas;
-
-        for (let k = 0; k < 3; k++) {
-            if (qtsMacas > 0) {
-                novoDia.professora[k] = "Maçã";
-                qtsMacas--;
-            } else
-            if (qtsPeras > 0) {
-                novoDia.professora[k] = "Pera";
-                qtsPeras--;
+            setFrutas(numFruta, numFruta2, strFruta,strFruta2)
+            {
+                for (let i = 0; i < 3; i++) {
+                    if (numFruta > 0) {
+                        this.frutas[i] = strFruta;
+                        numFruta--;
+                    } else
+                    if (numFruta2 > 0) {
+                        this.frutas[i] = strFruta2;
+                        numFruta2--;
+                    }
+            
+                } 
             }
         }
+};
+const popularVetMes = vetMes => {   
+    for (let i = 0; i < 30; i++)
+    {    
+        let novoDia = criarNovoDia();
+        novoDia.sortearQtdFrutas();
+        let {peras:qtsPeras,macas:qtsMacas} = novoDia;
+        if (novoDia.melhorAluno == "Maria") {        
+            novoDia.setFrutas(qtsPeras,qtsMacas,'Pera', 'Maçã')
+            }
+        else{ 
+             if (novoDia.melhorAluno == "João") {
+            novoDia.setFrutas(qtsMacas,qtsPeras,'Maçã','Pera')
+        }}
+    
+        vetMes.push(novoDia);
     }
+};
 
+const popularTabela = tbl => {
+    let row
+    for (let i = 1; i < 31; i++) {
+        diaAtual = mes[i - 1];
+        row = tbl.insertRow(i);
 
-    mes.push(novoDia);
-}
-
-//console.log(mes)
-
-for (let i = 1; i < 31; i++) {
-    var diaAtual = mes[i - 1];
-
-    row = tbl.insertRow(i);
-
-    for (let j = 0; j < 7; j++) {
-        cell = row.insertCell(j);
+    for (let i = 0; i < 7; i++) {
+        cell = row.insertCell(i);
     }
 
     tbl.rows[i].cells[0].innerHTML = i;
     tbl.rows[i].cells[1].innerHTML = diaAtual.peras;
     tbl.rows[i].cells[2].innerHTML = diaAtual.macas;
     tbl.rows[i].cells[3].innerHTML = diaAtual.melhorAluno;
-    tbl.rows[i].cells[4].innerHTML = diaAtual.professora[0];
+    tbl.rows[i].cells[4].innerHTML = diaAtual.frutas[0];
 
-
-    if (diaAtual.professora[1] == null)
+    if (diaAtual.frutas[1] == null)
         tbl.rows[i].cells[5].innerHTML = "--";
     else
-        tbl.rows[i].cells[5].innerHTML = diaAtual.professora[1];
+        tbl.rows[i].cells[5].innerHTML = diaAtual.frutas[1];
 
-    if (diaAtual.professora[2] == null)
+    if (diaAtual.frutas[2] == null)
         tbl.rows[i].cells[6].innerHTML = "--";
     else
-        tbl.rows[i].cells[6].innerHTML = diaAtual.professora[2];
-}
-
-for (let i = 0; i < mes.length; i++) {
-    for (let j = 0; j < mes[i].professora.length; j++) {
-        if (mes[i].professora[j] == "Maçã")
-            macasLevadas++;
-        else
-        if (mes[i].professora[j] == "Pera")
-            perasLevadas++;
+        tbl.rows[i].cells[6].innerHTML = diaAtual.frutas[2];
+}}
+const informaTotal = () => {
+    let macasLevadas = 0;
+    let perasLevadas = 0;
+    let tbl = document.getElementById("tabela")    
+    for (let i = 0; i < 30; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (mes[i].frutas[j] == "Maçã")
+                macasLevadas++;
+            else
+            if (mes[i].frutas[j] == "Pera")
+                perasLevadas++;
+        }
     }
+    
+    let result = document.getElementById('result');
+    
+    result.innerHTML = `Este mês a professora levou ${macasLevadas} maçãs e ${perasLevadas} peras.`;
 }
 
-let result = document.getElementById('result');
+popularVetMes(mes);
+popularTabela(tbl);
+informaTotal();
 
-result.innerHTML = `Este mês a professora levou ${macasLevadas} maçãs e ${perasLevadas} peras.`;
 
-//console.log(macasLevadas);
-//console.log(perasLevadas);
+
